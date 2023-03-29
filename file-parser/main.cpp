c4421746af71c44e2d60f26a519074e4e7103696
dgiantsidi <dimitra.giantsidi@gmail.com>
2023-03-29 20:08:51 +0100

    improve formatting

--- file-parser/main.cpp
+++ file-parser/main.cpp
@@ -1,16 +1,13 @@
+#include "file_parser.h"
+#include <QDebug>
 #include <QFile>
 #include <QStringList>
-#include <QDebug>
 #include <iostream>
-#include "file_parser.h"
 
+int main() {
+  file_parser a("file.csv");
 
-int main()
-{
-    file_parser a("file.csv");
+  a.parse_file();
 
-    a.parse_file();
-
-    return 0;
+  return 0;
 }
-
