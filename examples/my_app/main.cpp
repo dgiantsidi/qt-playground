cf176494b7cbcef35b7bda637a60a8a53cda75d5
dgiantsidi <dimitra.giantsidi@gmail.com>
2023-03-29 20:08:40 +0100

    rename

---
+++ examples/my_app/main.cpp
@@ -0,0 +1,19 @@
+#include "/home/dimitra/workspace/qt-example/qt-playground.git/file-parser/file_parser.h"
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+
+#if 0
+  file_parser c("file.csv");
+  c.parse_file();
+#endif
+  MainWindow w;
+#if 0
+  w.set_file_parser(&c);
+#endif
+  w.show();
+
+  return a.exec();
+}
